02ce85a46fd03c95c66300bb32abd9e2762db7b8
Administrator <Administrator@PMT2010>
2026-04-20 15:54:23 +0700

    hoan thanh

--- BUOI7/Account.h
+++ BUOI7/Account.h
@@ -1,1 +1,30 @@
-#pragma once
+#include <string>
+using namespace std;
+struct Account {
+	int id;
+	string lnk, usr, pwd, desc;
+	friend ostream& operator<<(ostream& os, Account& b) {
+		os << "Account information: " << endl;
+		os << "\t+ Link:" << b.lnk << endl;
+		os << "\t+ User Name:" << b.usr << endl;
+		os << "\t+ Password:" << b.pwd << endl;
+		os << "\t+ Description:" << b.desc << endl;
+		return os;
+	}
+	friend istream& operator>>(istream& in, Account& b) {
+		cout << "Account information: " << endl;
+		cout << "\t+ Id:";
+		in >> b.id;
+		cout << "\t+ Link: ";
+		in.ignore();
+		getline(in, b.lnk);
+		cout << "\t+ User name: ";
+		getline(in, b.usr);
+		cout << "\t+ Password: ";
+		getline(in, b.pwd);
+		cout << "\t+ Description:";
+		getline(in, b.desc);
+		return in;
+	}
+
+};
